673514304a9a6b2474a4078f3f3690d19285241a
tangsj <t_fate@163.com>
2015-04-17 00:06:25 +0800

    add uglify

--- project_zip/gulpfile.js
+++ project_zip/gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function(){
-    console.log('task');
+var basePath = 'D:/GitHub/work_tool/project_zip/';
+
+var path = {
+  js : 'lib/*.js'
+}
+
+gulp.task('compress', function() {
+  return gulp.src(basePath + path.js) // 指定源
+    .pipe(uglify())
+    .pipe(gulp.dest(basePath + '/dist')); // 指定输出
+});
+
+gulp.task('default', function(a){
+  gulp.start('compress');
 });
---
+++ project_zip/lib/gulp.js
@@ -0,0 +1,5 @@
+function gulp(){
+  alert("gulp");
+}
+
+gulp();
