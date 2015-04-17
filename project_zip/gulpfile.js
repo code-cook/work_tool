c7d0a563a097e7f8412392cac8aa1ea2b48a16e7
tangsj <t_fate@163.com>
2015-04-17 21:27:26 +0800

    add config.json

--- project_zip/gulpfile.js
+++ project_zip/gulpfile.js
@@ -2,17 +2,17 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-var basePath = 'D:/GitHub/work_tool/project_zip/';
+var config = require('./config.json');
 
 var path = {
   js : 'lib/*.js'
 }
 
 gulp.task('jsmin', function() {
-  return gulp.src(basePath + path.js) // 指定源
+  return gulp.src(config.jsSrc + path.js) // 指定源
     .pipe(uglify({mangle: true}))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(basePath + '/dist')); // 指定输出
+    .pipe(gulp.dest(config.jsSrc + '/dist')); // 指定输出
 });
 
 gulp.task('default', function(a){
