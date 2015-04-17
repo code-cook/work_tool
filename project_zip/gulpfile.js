1464b27000d0ec4d903d47848ec2b3d22731ef19
tangsj <t_fate@163.com>
2015-04-17 21:57:45 +0800

    uglify task

--- project_zip/gulpfile.js
+++ project_zip/gulpfile.js
@@ -1,20 +1,25 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var _ = require('lodash');
 
 var config = require('./config.json');
 
-var path = {
-  js : 'lib/*.js'
-}
-
 gulp.task('jsmin', function() {
-  return gulp.src(config.jsSrc + path.js) // 指定源
-    .pipe(uglify({mangle: true}))
+  var src_arr = [];
+  if(_.isArray(config.jsFilter) && config.jsFilter.length > 0){
+    _.each(config.jsFilter, function(){
+      src_arr.push(config.jsSrc + this);  
+    });
+  }else{
+    src_arr.push(config.jsSrc + '/**/*.js');
+  }
+  return gulp.src(src_arr) // 指定源
+    .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(config.jsSrc + '/dist')); // 指定输出
+    .pipe(gulp.dest(config.jsDist)); // 指定输出
 });
 
 gulp.task('default', function(a){
-  gulp.start('jsmin');
+  gulp.run('jsmin');
 });
