3a26734c9dc2ebde80fa7bcbe28ff9f78b25fafa
tangsj <t_fate@163.com>
2015-06-29 11:52:01 +0800

    es6 test

---
+++ es6-test/build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var a = 1;
+{
+  var _a = 0;
+  console.log(_a);
+}
+
+console.log(a);
+
+var x = 1;
+var y = 2;
+
+console.log(x);
+console.log(y);
---
+++ es6-test/src/index.js
@@ -0,0 +1,12 @@
+var a = 1;
+{
+  let a = 0;
+  console.log(a);
+}
+
+console.log(a);
+
+var [x, y] = [1, 2];
+
+console.log(x);
+console.log(y); 
