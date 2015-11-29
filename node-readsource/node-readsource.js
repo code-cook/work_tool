c1a48abe5792f4722522ec0b34159a0b4ac80d6a
tangsj <t_fate@163.com>
2015-11-29 20:33:09 +0800

    update node-readsource

---
+++ node-readsource/node-readsource.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var Path = require('path');
+
+var list = [], i = 0;
+
+var basePath = Path.join(__dirname, '/');
+/**
+ * 生成静态资源列表
+ */
+function loadDir(path, args){
+  !function(path){
+    var fun = arguments.callee;
+    var paths = fs.readdirSync(path);
+
+    paths.forEach(function(pathName, index){
+      var url =  Path.join(path, pathName);
+      if(fs.statSync(url).isDirectory()){
+        fun.call(this, url);
+      }else{
+        if(/\.png|\.jpg|\.gif$/.test(pathName)){
+          args.push({
+            src : url.replace(basePath, ''),
+            id: pathName.replace(/\.\w+$/, '')
+          });
+          i++;
+        }
+      }
+    });
+  }(path);
+}
+loadDir(Path.join(__dirname, 'images'), list);
+
+fs.writeFile('js/manifest.json', JSON.stringify(list, null, 2), function(){
+  console.log('manifest.json create success.');
+});
